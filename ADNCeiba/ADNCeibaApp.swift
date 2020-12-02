ddc30944cffc4ed81a19b23e9b358b9469f2b960
Matt Leon <mattleon@Matts-MacBook-Pro.local>
2020-12-02 15:50:17 -0500

    Initial template

---
+++ ADNCeiba/ADNCeibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADNCeibaApp.swift
+//  ADNCeiba
+//
+//  Created by Matt Leon on 2/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ADNCeibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
